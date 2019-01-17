f0e268dc4a82cd2f7c634351de9a83c3282d488f
lucapaterlini <support@crowdcoin.site>
2019-01-17 18:48:36 +0000

    fixed B C

--- B.py
+++ B.py
@@ -2,10 +2,22 @@ from merkletools import *
 
 mt = MerkleTools()
 
+list_data = ["a","b","c","b"]
+
+
+mt.add_leaf(list_data, True)
+mt.make_tree()
+
+print mt.get_merkle_root()
+print "14ede5e8e97ad9372327728f5099b95604a39593cac3bd38a343ad76205213e7"
+
+mt = MerkleTools()
+
 list_data = ["a","b","c"]
 
 
 mt.add_leaf(list_data, True)
 mt.make_tree()
 
-print "root:", mt.get_merkle_root()
+print mt.get_merkle_root()
+print "e76328b6ca10676c686a0d534e8222ad8da04fdfe14c6f6ff67d08cbbd24c605"
--- C.py
+++ C.py
@@ -206,4 +206,8 @@ class MerkleTree:
         return self._get_branch_by_hash(hash_)
 
 
+print MerkleTree(["a","b","c","d"]).root_hash
+print "14ede5e8e97ad9372327728f5099b95604a39593cac3bd38a343ad76205213e7"
+
 print MerkleTree(["a","b","c"]).root_hash
+print "e76328b6ca10676c686a0d534e8222ad8da04fdfe14c6f6ff67d08cbbd24c605"
